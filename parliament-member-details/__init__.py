261bbbce8d21bd9a6f80c988f08a37a180daff9b
Alex Antal <alexxei@gmail.com>
2022-02-21 23:49:07 +0200

    handle-exceptions-members-of-parliament-details

--- parliament-member-details/__init__.py
+++ parliament-member-details/__init__.py
@@ -15,6 +15,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     chamber = req.params.get("cham")
     member_id = req.params.get("id")
 
+    # handle any missing parameter
     if not all([legislature, chamber, member_id]):
         return func.HttpResponse(
             "Please enter values for 'leg' (legislature), 'cham' (chamber) "
@@ -22,11 +23,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             status_code=406,
         )
 
+    # handle wrong chamber value
+    if chamber not in ["1","2"]:
+        return func.HttpResponse(
+            "'cham' (chamber) value should be '1' or '2'",
+            status_code=406,
+        )
+
+
     link = "http://www.cdep.ro/pls/parlam/structura2015.mp?idm={}&cam={}&leg={}".format(  # noqa: E501
         member_id, chamber, legislature
     )
 
     req = requests.get(link)
+
+    # handle wrong legislature value
+    if req.status_code == 404:
+        return func.HttpResponse(
+            "Please enter a correct year value for leg (legislature).",
+            status_code=406
+        )                  
+
+    # get the right encoding
     http_encoding = (
         req.encoding
         if "charset" in req.headers.get("content-type", "").lower()
@@ -34,9 +52,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     )
     html_encoding = EncodingDetector.find_declared_encoding(req.content, is_html=True)
     encoding = html_encoding or http_encoding
+    
     soup = BeautifulSoup(req.content, "lxml", from_encoding=encoding)
     to_return = {}
+    
     name = soup.find("title").text
+    
+    # handle wrong idm
+    if not name.strip():
+        return func.HttpResponse(
+            "Wrong id (parliament member id).",
+            status_code=406
+        )
+
     profile_div = soup.find("div", attrs={"class": "profile-pic-dep"})
     photo_link = "http://www.cdep.ro" + profile_div.find("img")["src"]
     birth_date = profile_div.text.strip()[2:].strip()
